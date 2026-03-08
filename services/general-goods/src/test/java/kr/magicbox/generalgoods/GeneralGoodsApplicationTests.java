bd9c0803c3e8cac0874412b6b70984b92ec766fd
Lian08 <lian080408@gmail.com>
2026-03-08 12:53:13 +0900

    feat/39 :: general-goods 서비스 추가

---
+++ services/general-goods/src/test/java/kr/magicbox/generalgoods/GeneralGoodsApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.magicbox.generalgoods;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeneralGoodsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
