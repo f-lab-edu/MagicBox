c3e0f911b72a72ab465b4982a9c705cff9be654c
Lian08 <lian080408@gmail.com>
2026-03-12 20:00:03 +0900

    feat/42 :: 활성 기기수 초과 예외 추가

---
+++ services/user/src/main/java/kr/magicbox/user/domain/exception/ActiveDeviceLimitExceededException.java
@@ -0,0 +1,11 @@
+package kr.magicbox.user.domain.exception;
+
+import kr.magicbox.user.domain.constants.UserPolicyConstants;
+import kr.magicbox.user.global.exception.BusinessException;
+import org.springframework.http.HttpStatus;
+
+public class ActiveDeviceLimitExceededException extends BusinessException {
+    public ActiveDeviceLimitExceededException() {
+        super("활성 기기 수 제한(" + UserPolicyConstants.maxActiveDevicesPerUser + "개)을 초과했습니다. 기존 기기에서 로그아웃 후 다시 시도해주세요.", HttpStatus.BAD_REQUEST);
+    }
+}
