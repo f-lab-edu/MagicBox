ad91d0c911a1a67cde14ea3a8fedca24a40a5443
Lian08 <lian080408@gmail.com>
2026-03-17 14:28:30 +0900

    feat/42 :: SecurityConfiguration 추가 및 SecurityFilterChain에 추가하도록 변경

--- services/user/src/main/java/kr/magicbox/user/global/filter/UserInfoExtractFilter.java
+++ services/user/src/main/java/kr/magicbox/user/global/filter/UserInfoExtractFilter.java
@@ -6,21 +6,16 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import kr.magicbox.user.domain.enums.UserRole;
 import lombok.NonNull;
-import org.springframework.core.Ordered;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.core.annotation.Order;
-import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 import java.util.Collection;
 import java.util.Collections;
 
-@Component
-@Order(Ordered.HIGHEST_PRECEDENCE)
 public class UserInfoExtractFilter extends OncePerRequestFilter {
 
     @Override
