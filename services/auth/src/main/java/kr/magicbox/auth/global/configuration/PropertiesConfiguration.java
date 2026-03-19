96df85965d234142a4bbc6668fb2ef42096491b3
Lian08 <lian080408@gmail.com>
2026-03-19 13:33:50 +0900

    feat/34 :: PropertiesConfiguration 디렉터리 범위 수정 완료

--- services/auth/src/main/java/kr/magicbox/auth/global/configuration/PropertiesConfiguration.java
+++ services/auth/src/main/java/kr/magicbox/auth/global/configuration/PropertiesConfiguration.java
@@ -4,6 +4,6 @@ import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ConfigurationPropertiesScan(basePackages = "kr.magicbox.auth.global.properties")
+@ConfigurationPropertiesScan(basePackages = "kr.magicbox.auth")
 public class PropertiesConfiguration {
 }
