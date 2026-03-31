8680a852c382e6427a7767546f4dfa722b7d61b3
Lian08 <lian080408@gmail.com>
2026-04-01 08:08:24 +0900

    feat/35 :: LogoutEventPublisherPort 제거

--- services/auth/src/main/java/kr/magicbox/auth/application/port/out/LogoutEventPublisherPort.java
+++
@@ -1,7 +0,0 @@
-package kr.magicbox.auth.application.port.out;
-
-import kr.magicbox.auth.domain.event.UserLoggedOutEvent;
-
-public interface LogoutEventPublisherPort {
-    void publish(UserLoggedOutEvent event);
-}
