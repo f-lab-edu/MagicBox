1a2d1f8c2efc86c320381181453288642dd31a91
Lian08 <lian080408@gmail.com>
2026-04-01 07:57:14 +0900

    feat/35 :: Long에서 UserId로 @AuthenticationPrincipal 변경

--- services/auth/src/main/java/kr/magicbox/auth/adapter/in/security/filter/UserInfoExtractFilter.java
+++ services/auth/src/main/java/kr/magicbox/auth/adapter/in/security/filter/UserInfoExtractFilter.java
@@ -5,6 +5,7 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import kr.magicbox.auth.adapter.in.security.properties.TrustedIpProperties;
+import kr.magicbox.auth.domain.vo.UserId;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -34,7 +35,8 @@ public class UserInfoExtractFilter extends OncePerRequestFilter {
             return;
         }
 
-        Long userId = Long.valueOf(userIdRequestHeader);
+        Long userIdLong = Long.valueOf(userIdRequestHeader);
+        UserId userId = UserId.of(userIdLong);
 
         UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(userId, null);
         SecurityContextHolder.getContext().setAuthentication(authenticationToken);
