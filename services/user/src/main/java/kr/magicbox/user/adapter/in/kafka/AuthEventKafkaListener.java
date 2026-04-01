5c33ec0460134edba52fa30febe6c413e023beb6
Lian08 <lian080408@gmail.com>
2026-04-01 22:23:39 +0900

    feat/35 :: 이벤트 Payload 구조 변화에 따른 코드 변경

--- services/user/src/main/java/kr/magicbox/user/adapter/in/kafka/AuthEventKafkaListener.java
+++ services/user/src/main/java/kr/magicbox/user/adapter/in/kafka/AuthEventKafkaListener.java
@@ -3,13 +3,10 @@ package kr.magicbox.user.adapter.in.kafka;
 import kr.magicbox.user.adapter.in.kafka.event.LoginEvent;
 import kr.magicbox.user.adapter.in.kafka.event.LogoutEvent;
 import kr.magicbox.user.application.port.in.ManageUserSessionUseCase;
-import kr.magicbox.user.domain.vo.UserId;
 import lombok.RequiredArgsConstructor;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
-
-import java.time.Instant;
 
 @Component
 @RequiredArgsConstructor
@@ -18,13 +15,13 @@ public class AuthEventKafkaListener {
 
     @KafkaListener(topics = "outbox.event.user-logged-in", groupId = "user-service")
     public void handleLoginEvent(ConsumerRecord<String, LoginEvent> record) {
-        Instant eventAt = Instant.ofEpochMilli(record.timestamp());
-        manageUserSessionUseCase.startSession(UserId.of(record.value().userId()), eventAt);
+        LoginEvent loginEvent = record.value();
+        manageUserSessionUseCase.startSession(loginEvent.userId(), loginEvent.createdAt());
     }
 
     @KafkaListener(topics = "outbox.event.user-logged-out", groupId = "user-service")
     public void handleLogoutEvent(ConsumerRecord<String, LogoutEvent> record) {
-        Instant eventAt = Instant.ofEpochMilli(record.timestamp());
-        manageUserSessionUseCase.endSession(UserId.of(record.value().userId()), eventAt);
+        LogoutEvent logoutEvent = record.value();
+        manageUserSessionUseCase.endSession(logoutEvent.userId(), logoutEvent.createdAt());
     }
 }
--- services/user/src/main/java/kr/magicbox/user/adapter/in/kafka/event/LogoutEvent.java
+++ services/user/src/main/java/kr/magicbox/user/adapter/in/kafka/event/LogoutEvent.java
@@ -1,8 +1,13 @@
 package kr.magicbox.user.adapter.in.kafka.event;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import kr.magicbox.user.domain.vo.UserId;
 import lombok.Builder;
 
+import java.time.Instant;
+
 @Builder
-public record LogoutEvent(@JsonProperty("value") Long userId) {
-}
+public record LogoutEvent(
+        @JsonProperty("user_id") UserId userId,
+        @JsonProperty("created_at") Instant createdAt
+) {}
