e0ac1c8676957ea7e5788fce1fcb4c4ea7dd465c
Lian08 <lian080408@gmail.com>
2026-04-01 09:36:53 +0900

    feat/35 :: UserId로 변경

--- services/user/src/main/java/kr/magicbox/user/adapter/in/kafka/AuthEventKafkaListener.java
+++ services/user/src/main/java/kr/magicbox/user/adapter/in/kafka/AuthEventKafkaListener.java
@@ -3,6 +3,7 @@ package kr.magicbox.user.adapter.in.kafka;
 import kr.magicbox.user.adapter.in.kafka.event.LoginEvent;
 import kr.magicbox.user.adapter.in.kafka.event.LogoutEvent;
 import kr.magicbox.user.application.port.in.ManageUserSessionUseCase;
+import kr.magicbox.user.domain.vo.UserId;
 import lombok.RequiredArgsConstructor;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.kafka.annotation.KafkaListener;
@@ -18,12 +19,12 @@ public class AuthEventKafkaListener {
     @KafkaListener(topics = "outbox.event.user-logged-in", groupId = "user-service")
     public void handleLoginEvent(ConsumerRecord<String, LoginEvent> record) {
         Instant eventAt = Instant.ofEpochMilli(record.timestamp());
-        manageUserSessionUseCase.startSession(record.value().userId(), eventAt);
+        manageUserSessionUseCase.startSession(UserId.of(record.value().userId()), eventAt);
     }
 
     @KafkaListener(topics = "outbox.event.user-logged-out", groupId = "user-service")
     public void handleLogoutEvent(ConsumerRecord<String, LogoutEvent> record) {
         Instant eventAt = Instant.ofEpochMilli(record.timestamp());
-        manageUserSessionUseCase.endSession(record.value().userId(), eventAt);
+        manageUserSessionUseCase.endSession(UserId.of(record.value().userId()), eventAt);
     }
 }
