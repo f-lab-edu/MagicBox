38d848b7ed23e05b8348e89ac571b51fe3dcd44b
Lian08 <lian080408@gmail.com>
2026-04-03 09:31:30 +0900

    refactor/35 :: event 관련 추가

---
+++ services/user/src/main/java/kr/magicbox/user/domain/event/UserDomainEventType.java
@@ -0,0 +1,14 @@
+package kr.magicbox.user.domain.event;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum UserDomainEventType {
+
+    USER_WITHDRAWN("user-withdrawn"),
+    USER_SIGNUP("user-signup");
+
+    private final String value;
+}
