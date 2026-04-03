07cde3a407dc3efa15de53af5bca25e4be414329
Lian08 <lian080408@gmail.com>
2026-04-03 12:01:28 +0900

    refactor/35 :: Ban 관련 이벤트 추가

--- services/user/src/main/java/kr/magicbox/user/application/service/BanUserService.java
+++ services/user/src/main/java/kr/magicbox/user/application/service/BanUserService.java
@@ -1,19 +1,24 @@
 package kr.magicbox.user.application.service;
 
 import kr.magicbox.user.application.port.in.BanUserUseCase;
+import kr.magicbox.user.application.port.out.UserDomainEventRepositoryPort;
 import kr.magicbox.user.application.port.out.UserRepositoryPort;
 import kr.magicbox.user.domain.aggregate.User;
+import kr.magicbox.user.domain.event.UserBannedEvent;
 import kr.magicbox.user.domain.exception.UserNotFoundException;
 import kr.magicbox.user.domain.vo.Nickname;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.Instant;
+
 @Service
 @RequiredArgsConstructor
 public class BanUserService implements BanUserUseCase {
 
     private final UserRepositoryPort userRepositoryPort;
+    private final UserDomainEventRepositoryPort userDomainEventRepositoryPort;
 
     @Override
     @Transactional
@@ -23,5 +28,11 @@ public class BanUserService implements BanUserUseCase {
 
         user.accountDeactivate();
         userRepositoryPort.updateUser(user);
+
+        UserBannedEvent event = UserBannedEvent.builder()
+                .userId(user.getId())
+                .bannedAt(Instant.now())
+                .build();
+        userDomainEventRepositoryPort.save(event);
     }
 }
