99faffe48ea9780826fec9be3b30d50c71c49dcf
Lian08 <lian080408@gmail.com>
2026-04-16 14:03:36 +0900

    refactor/104 :: auth/creator/user 서비스에 ForwardedHeaderFilter 등록

--- services/creator/src/main/java/kr/magicbox/creator/adapter/in/security/configuration/SecurityConfiguration.java
+++ services/creator/src/main/java/kr/magicbox/creator/adapter/in/security/configuration/SecurityConfiguration.java
@@ -1,10 +1,7 @@
-package kr.magicbox.auth.adapter.in.security.configuration;
+package kr.magicbox.creator.adapter.in.security.configuration;
 
-import kr.magicbox.auth.adapter.in.security.filter.UserInfoExtractFilter;
-import kr.magicbox.auth.adapter.in.security.oauth2.MagicBoxOAuth2UserService;
-import kr.magicbox.auth.adapter.in.security.oauth2.OAuth2LoginFailureHandler;
-import kr.magicbox.auth.adapter.in.security.oauth2.OAuth2LoginSuccessHandler;
-import kr.magicbox.auth.adapter.in.security.properties.TrustedIpProperties;
+import kr.magicbox.creator.adapter.in.security.filter.UserInfoExtractFilter;
+import kr.magicbox.creator.adapter.in.security.properties.TrustedIpProperties;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,8 +9,11 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.filter.ForwardedHeaderFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -21,9 +21,16 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 public class SecurityConfiguration {
 
     private final TrustedIpProperties trustedIpProperties;
-    private final MagicBoxOAuth2UserService magicBoxOAuth2UserService;
-    private final OAuth2LoginSuccessHandler oAuth2LoginSuccessHandler;
-    private final OAuth2LoginFailureHandler oAuth2LoginFailureHandler;
+
+    @Bean
+    public ForwardedHeaderFilter forwardedHeaderFilter() {
+        return new ForwardedHeaderFilter();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return new InMemoryUserDetailsManager();
+    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) {
@@ -32,13 +39,6 @@ public class SecurityConfiguration {
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .addFilterBefore(new UserInfoExtractFilter(trustedIpProperties), UsernamePasswordAuthenticationFilter.class)
                 .authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
-                .oauth2Login(oauth2 -> oauth2
-                        .redirectionEndpoint(redirection -> redirection
-                                .baseUri("/oauth2/callback/*"))
-                        .userInfoEndpoint(userInfo -> userInfo
-                                .userService(magicBoxOAuth2UserService))
-                        .successHandler(oAuth2LoginSuccessHandler)
-                        .failureHandler(oAuth2LoginFailureHandler))
                 .build();
     }
 }
