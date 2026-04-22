80afad7c679429313d60ca9c01a589051ad2f42b
Lian08 <lian080408@gmail.com>
2026-04-22 21:26:57 +0900

    feat/100 :: auth: OutboxAdapter로 리네이밍 및 관련 파일 삭제

--- services/auth/src/main/java/kr/magicbox/auth/application/port/out/AuthDomainEventRepositoryPort.java
+++
@@ -1,7 +0,0 @@
-package kr.magicbox.auth.application.port.out;
-
-import kr.magicbox.auth.domain.event.AuthDomainEvent;
-
-public interface AuthDomainEventRepositoryPort {
-    void save(AuthDomainEvent event);
-}
