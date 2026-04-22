8ab4f2feb703ca45ed4204658060e5211e54db95
Lian08 <lian080408@gmail.com>
2026-04-22 22:29:48 +0900

    fix: CreatorAlreadyBannedException BaseException 직접 상속 (继承层级 감소)

--- services/creator/src/main/java/kr/magicbox/creator/domain/exception/CreatorAlreadyBannedException.java
+++ services/creator/src/main/java/kr/magicbox/creator/domain/exception/CreatorAlreadyBannedException.java
@@ -1,9 +1,9 @@
 package kr.magicbox.creator.domain.exception;
 
-import kr.magicbox.creator.global.exception.BusinessException;
+import kr.magicbox.creator.global.exception.BaseException;
 import org.springframework.http.HttpStatus;
 
-public class CreatorAlreadyBannedException extends BusinessException {
+public class CreatorAlreadyBannedException extends BaseException {
 
     public CreatorAlreadyBannedException() {
         super("이미 밴 처리된 크리에이터입니다.", HttpStatus.CONFLICT);
