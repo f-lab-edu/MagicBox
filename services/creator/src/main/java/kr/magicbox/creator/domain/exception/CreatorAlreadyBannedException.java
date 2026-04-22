97aec6e5c4c254cee25606b5c154bdd1c9224401
Lian08 <lian080408@gmail.com>
2026-04-22 22:19:00 +0900

    fix: CreatorAlreadyBannedException BusinessException 상속으로 복원

--- services/creator/src/main/java/kr/magicbox/creator/domain/exception/CreatorAlreadyBannedException.java
+++ services/creator/src/main/java/kr/magicbox/creator/domain/exception/CreatorAlreadyBannedException.java
@@ -1,9 +1,9 @@
 package kr.magicbox.creator.domain.exception;
 
-import kr.magicbox.creator.global.exception.BaseException;
+import kr.magicbox.creator.global.exception.BusinessException;
 import org.springframework.http.HttpStatus;
 
-public class CreatorAlreadyBannedException extends BaseException {
+public class CreatorAlreadyBannedException extends BusinessException {
 
     public CreatorAlreadyBannedException() {
         super("이미 밴 처리된 크리에이터입니다.", HttpStatus.CONFLICT);
