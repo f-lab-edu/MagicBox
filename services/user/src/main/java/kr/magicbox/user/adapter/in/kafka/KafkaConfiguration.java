5ed4667d67e655146694bcc979ba2b3dd7a78ba4
Lian08 <lian080408@gmail.com>
2026-04-23 07:59:29 +0900

    fix: user KafkaConfiguration 오류 수정 (KafkaRetryProperties -> KafkaProperties)

--- services/user/src/main/java/kr/magicbox/user/adapter/in/kafka/KafkaConfiguration.java
+++ services/user/src/main/java/kr/magicbox/user/adapter/in/kafka/KafkaConfiguration.java
@@ -1,7 +1,5 @@
 package kr.magicbox.user.adapter.in.kafka;
 
-import kr.magicbox.user.adapter.in.kafka.properties.KafkaRetryProperties;
-import kr.magicbox.user.adapter.out.persistence.repository.UserInboxRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
@@ -21,19 +19,20 @@ import org.springframework.util.backoff.ExponentialBackOff;
 @Configuration
 @RequiredArgsConstructor
 public class KafkaConfiguration {
-    private final KafkaRetryProperties kafkaRetryProperties;
-    private final UserInboxRepository userInboxRepository;
+    private final KafkaProperties kafkaProperties;
 
     @Bean
     public CommonErrorHandler errorHandler(KafkaTemplate<?, ?> kafkaTemplate) {
+        KafkaProperties.Retry.Topic retryTopic = kafkaProperties.getRetry().getTopic();
+        ExponentialBackOff backOff = new ExponentialBackOff(
+                retryTopic.getBackoff().getDelay().toMillis(),
+                retryTopic.getBackoff().getMultiplier()
+        );
+        backOff.setMaxAttempts(retryTopic.getAttempts());
+
         DeadLetterPublishingRecoverer recoverer = new DeadLetterPublishingRecoverer(kafkaTemplate,
                 (ConsumerRecord<?, ?> record, Exception ex) -> {
                     log.error("[DLT] 메시지 처리 실패, DLT 전송합니다. topic={}, offset={}, exception={}", record.topic(), record.offset(), ex.getMessage());
-                    userInboxRepository.findByTopicAndPartitionAndOffset(record.topic(), record.partition(), record.offset())
-                            .ifPresent(inbox -> {
-                                inbox.markDeadLettered();
-                                userInboxRepository.save(inbox);
-                            });
                     return new TopicPartition(record.topic() + "-dlt", record.partition());
                 });
         return new DefaultErrorHandler(recoverer, backOff);
